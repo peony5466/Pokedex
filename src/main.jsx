262f83863cabc37f6d4ef69014cbd5b194845b8f
Hiba Sahbane <98028797+peony5466@users.noreply.github.com>
2026-01-30 10:09:47 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+import React ,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
+import { LanguageProvider } from './context/LanguageContext.jsx'
 import App from './App.jsx'
 
+
+
+const queryClient = new QueryClient();
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <QueryClientProvider client = {queryClient}>
+      <LanguageProvider>
+       <App />
+      </LanguageProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
